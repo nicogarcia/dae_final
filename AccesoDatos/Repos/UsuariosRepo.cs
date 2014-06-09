3df40d13f5bddcb973a27e989223f9d3e42b0f6b
julio jose maldonado <vessal27@hotmail.com>
2014-06-09 03:17:54 -0300

    Parte de usuario

---
+++ AccesoDatos/Repos/UsuariosRepo.cs
@@ -0,0 +1,37 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class UsuariosRepo
+    {
+        private ReservasContext reservasContext;
+
+       
+        public UsuariosRepo()
+        {
+
+        }
+        public UsuariosRepo(ReservasContext reservasContext)
+        {
+            // TODO: Complete member initialization
+            this.reservasContext = reservasContext;
+        }
+
+        public IList<Usuario> Todos ()
+        {
+            return reservasContext.Usuarios.ToList();
+        }
+
+        public void AgregarUsuario(Usuario usuario)
+        {
+            reservasContext.Usuarios.Add(usuario);
+            reservasContext.SaveChanges();
+
+        }
+    }
+}
