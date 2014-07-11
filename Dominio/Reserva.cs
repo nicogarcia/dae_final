4b435c34994a4b17d608a56e55759e0bcc5186bc
julio jose maldonado <vessal27@hotmail.com>
2014-07-11 03:26:25 -0300

    Modelo Reserva

---
+++ Dominio/Reserva.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio
+{
+    public class Reserva
+    {
+        public int Id { get; private set; }
+
+        [Required]
+        public Usuario Creador { get; set; }
+
+        [Required]
+        public Usuario Responsable { get; set; }
+
+        [Required]
+        public DateTime Inicio { get; set; }
+
+        [Required]
+        public DateTime Fin { get; set; }
+
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public Recurso RecursoReservado { get; set; }
+
+
+        public EstadoReserva Estado { get; set; }
+
+        public Reserva()
+        { }
+    }
+}
