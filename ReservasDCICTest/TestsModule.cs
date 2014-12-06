125fd96402e4cb9c8a949e83fd845951b5b967ed
nicogarcia <nicoalbo90@gmail.com>
2014-12-06 20:15:06 -0300

    Unit test con Ninject.MockingKernel.Moq funcionando!

---
+++ ReservasDCICTest/TestsModule.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Queries;
+using Dominio.Repos;
+using Dominio.UnitOfWork;
+using Dominio.Validacion;
+using Ninject.MockingKernel;
+using Ninject.Modules;
+using Presentacion.Controllers;
+using Presentacion.Models.Conversores;
+
+namespace ReservasDCICTest
+{
+    class TestsModule : NinjectModule
+    {
+        
+        public override void Load()
+        {
+            /*Bind<ITiposDeCaracteristicasRepo>().ToMock();
+            Bind<ITiposDeRecursosRepo>().ToMock();
+            Bind<IUnitOfWorkFactory>().ToMock();
+            Bind<IConversorRecurso>().ToMock();
+            Bind<IValidadorDeRecursos>().ToMock();
+            Bind<IRecursosQueriesTS>().ToMock();
+            Bind<IMultipleTypeQueriesTS>().ToMock();*/
+
+            Bind<RecursosController>().ToMock();
+        }
+    }
+}
